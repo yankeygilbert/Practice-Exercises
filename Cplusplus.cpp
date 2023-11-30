f505afb3b8dd7544adff147bd8e784fdec233a29
yankeygilbert <gilbertyankey38@gmail.com>
2023-11-30 06:37:23 +0000

    update

--- Cplusplus.cpp
+++ Cplusplus.cpp
@@ -1,180 +1,52 @@
 #include <iostream>
-#define arrlim 100
+#include <iomanip>
+#include <fstream>
+#include "headerfile.h"
 
- template <typename Type>
- Type equal(Type &val1,Type &val2){
-    if(sizeof(Type) != 2 || sizeof(Type) != 4){
-      return (val1=val2);
-    }
-   if(sizeof(Type) == 4 || sizeof(Type)==2 ){
-     
-    }
- }
-
- template <typename classtype>
- class testing{
-    public:
-      classtype add(classtype val1,classtype val2);
-  };
-
-  template<typename classtype>
-  classtype testing<classtype>::add(classtype val1,classtype val2){
-
-  }
-
-// class definition
-class clockType{
-  private:
-    int hr,min,sec;
-    std::string tzone;
-  
-  public:
-    void settime(int,int,int);
-    void printTime() const;
-    void timer(int,int,int) const;
-    bool equaltime(const clockType &) const;
-  
-    clockType();
-    
-};
-
-//class implementation
-
-clockType::clockType(){
-  hr=0;
-  min=0;
-  sec=0;
- 
-}
-
-void clockType::settime(int hour,int mins,int secs){
-   hr = hour;
-   min = mins;
-   sec = secs;
-}
-
-void clockType::printTime() const {
-    std::cout<<hr<<":"<<min<<":"<<sec<<std::endl;
-}
-
-void clockType::timer(int hour, int mins, int secs) const{
-    for(hour; hour>=0 && hour<12; hour--){
-      for(mins; mins>=0 && mins<60;mins-- ){
-        for(secs;secs>=0 && secs<60;secs--){
-           std::cout<<hour+":"<<mins+":"<<secs<<std::endl;
-        } 
-      }
-    }
-}
-bool clockType::equaltime(const clockType &compClock) const {
-  return(hr == compClock.hr && min == compClock.min && sec == compClock.sec);
-}
-
-// Structure definition 
-struct studenType{
-  
-  std::string home;
-  std::string type;
-  std::string address;
-  std::string func1 (){
-    std::cout<<"you made it "<<std::endl;
-  }
-     
-};
-
-void collect(studenType student[])
-{
-  std::cout <<student[1].home<<std::endl;
-}
-
-struct studenType student(studenType &structvar){
-  return structvar;
-}
-
-//Derived class definition
-class timeZone:public clockType{
-  public:
-    
-    std::string zone;
-    int *time(int *,int *,int *);
-    void settime(int,int,int,std::string) ;
-    timeZone();
-    
- 
-};
-
-// derived class implementaiton
-
-int* timeZone::time(int *hour,int *mins,int *secs){
-
-  int *p[3]={hour,mins,secs};
- 
-  return *p;
-}
-void timeZone::settime(int hour,int min,int sec,std::string tzone)  {
-    clockType::settime(hour,min,sec);
-    zone = tzone;
-}
-
-//constructor 
-timeZone::timeZone(){
-  clockType::settime(1,2,5);
-   zone= "africa";
-}
-
- class functionOverload{
-    public:
-     
-      functionOverload(int*,int*,int*);
-      ~functionOverload();
-      friend functionOverload operator+ (const functionOverload&,const functionOverload&);
-
-     private:
-       int val1,val2,val3;
- };
-
- functionOverload::~functionOverload(){
-  
- }
- functionOverload::functionOverload(int *v1,int *v2, int* v3){
-  val1= *v1;
-  val2= *v2;
-  val3 =*v3;
-  std::cout<< v1 <<":"<< val1 <<std::endl;
-
- }
-
-  functionOverload operator+ (const functionOverload &otherClass1, const functionOverload &othercalss2) {
-
-  }
 
 main(){
   timeZone Time;
   clockType time2;
   clockType timeess;
-  studenType guess[arrlim];
-  studenType clen;
 
-  int hour=3;
-  int min=6;
-  int sec=45;
- timeess.settime(0,0,0);
- time2.settime(1,2,3);
- equal<clockType>(time2,timeess);
+  std::string hey;
+
+  int hour=7;
+  int mins=3;
+  int sec=5;
+  
+  timeess.settime(0,0,0);
+  time2.settime(1,2,3);
+  equal<clockType>(time2,timeess);
+  time2.printTime();
+
+ 
+ int *collecttime = Time.time(&hour,&mins,&sec);
  time2.printTime();
+ std::cout << collecttime[1]<<std::endl;
+ Time.equaltime(time2) ? std::cout<<"E"<<std::endl : std::cout<<"F"<<std::endl;
 
-   //guess[1] = {"anaji","young","12333"};
-   
-  //collect(guess);
- // student(clen);
- //int *collecttime = Time.time(&hour,&min,&sec);
- //time2.printTime();
- //std::cout << collecttime[1]<<std::endl;
- //Time.equaltime(time2) ? std::cout<<"E"<<std::endl : std::cout<<"F"<<std::endl;
+ 
+  std::ofstream file;
+  std::ifstream filein;
 
-  //functionOverload obj1(&hour,&min,&sec);
+  file.open("home.txt",std::ios::app);
+  filein.open("home.txt");
 
+  file<<"halleujah\n"<<std::endl;
 
+  filein>>hey;
+
+  std::cout<<hey<<std::endl;
+  functionOverload obj;
+  functionOverload t1(&hour,&mins,&sec);
+  functionOverload t2(&hour,&mins,&sec);
+
+ file.close();
+ filein.close();
+ 
+ std::cout<<argvet('a','b')<<std::endl;
+ obj= t1 + t2;
+ std::cout<<obj.val3;
  return 0;
-
 }
--- python.py
+++ python.py
@@ -1,10 +1,12 @@
 from collections import deque
+import math
+import json
 def my_function():
      '''home homie\n
         we  are happy here'''
 #print(my_function.__doc__)
 
-def functon(prompt, home="shit"):
+def functon(prompt, home='shit'):
     value = input(prompt)
     return lambda: print(home+" "+value)
 a,b=2,3
@@ -20,10 +22,59 @@ qr = [(x,y) for x in[1,2,3] for y in[3,1,4] if x==y]
 print(squares)
 print(qr)
 
-vec = [-4, -2, 0, 2, 4]
+vec = [-4, 0]
 
 vec= [x for x in vec if x>=0]
 print (vec)
 
-tupes = ('home',)
-print(len(tupes))
+tel = {'jack':42,'mate':75,'doc':66}
+tel['home']=100
+print(tel)
+print(sorted(tel))
+
+suss = dict([(4,2),(2,100)])
+
+print(suss)
+v = set('adbcdde')
+qt= set('abufusein')
+
+print(sorted(v|qt))
+
+dicta = {x: x**2 for x in range(10)}
+print(dicta)
+
+print(dict(sape=14,magor=74))
+
+questions = ['name', 'quest', 'favorite color']
+answers = ['lancelot', 'the holy grail', 'blue']
+for q, a in zip(questions, answers):
+    print('What is your {0}? It is {1}.'.format(q, a))
+
+for i in reversed(range(1,11)):
+    print (i)
+val,vex =2,3
+
+string=['happy me','house']
+print(f'shit{val},shit2{vex}')
+print('shit{0} shir2{1}'.format(val,vex))
+
+print(repr((qt,('home','steve'))))
+
+
+
+with open('file.txt','w') as file:
+    json.dump(string,file)
+
+file.close() 
+
+file =  open('file.txt','r')
+#json.load(file)
+#print(f' {json.load(file)}')
+
+for i in enumerate(json.load(file)):
+    print('{}'.format(str(i)))
+
+file.close()
+
+hope ='svare'
+print('{!r}'.format(hope))
