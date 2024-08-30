0cf5b6cd5eb602aa2194fc9b0e03258a84c7c46f
yankeygilbert <gilbertyankey38@gmail.com>
2024-08-30 10:06:02 +0000

    upated

--- Health_Management_Software.py
+++ Health_Management_Software.py
@@ -1,7 +1,7 @@
-#Acessing date - time functionality
 from datetime import date
-#<---------------------------------------------------------------------------->#
+
 #Functionality Implemented Here
+#class starts Here
 class HealthAnalyzer:
     #function to collect User Information
     def __init__(self):
@@ -11,15 +11,11 @@ class HealthAnalyzer:
         self.firstname = str(input("Enter Your FirstName \n"))
         self.lastname  = str(input("Enter your LastName \n"))
         self.DOB = str(input("Please Enter date of birth in YYYY-MM-DD \n"))
-        self.weight = int(input("Enter your Weight on KG \n"))
-        self.Height  = int(input("Enter your Height \n"))
-        self.ChlLevel  = int(input("Enter your Cholestrol Level \n"))
+        self.weight = float(input("Enter your Weight on KG \n"))
+        self.Height  = float(input("Enter your Height \n"))
+        self.ChlLevel  = float(input("Enter your Cholestrol Level \n"))
         self.DateToday = date.today()
-        
-    
-    #function to store User Information   
-    def storeInfo(self):
-        #Storing Patient in Dictionary
+          #Storing Patient in Dictionary
         self.PatientRecord = {
             "first_name" : self.firstname, 
             "last_name" : self.lastname,
@@ -29,62 +25,65 @@ class HealthAnalyzer:
             "Chl_Level" :self.ChlLevel,
         }
 
-        #store data in file for reference
-        with open('PatientData.txt','a') as DB:
-            DB.write(str(self.PatientRecord)+"\n")
-
-       #creating an empty list
-        self.PatientDB = []
         #appendidng to list 
-        self.PatientDB.append(self.PatientRecord)
-       
+        Patientdb.append(self.PatientRecord)
+        
+
     #function perform analysis 
     def healthAnalysis(self):
         #Get Patient Record
-        for index in range(0,len(self.PatientDB)):
-            if (self.lastname ) == self.PatientDB[index]['last_name']:
-                self.Record = self.PatientDB[index]
-        
-            else:
-                # this Exits if users last name does not exist in Db
-                print("User Not In Patient Records")
-                exit
+            
+                self.Record = self.PatientRecord
+                self.Age = int(self.DateToday.year) - int(self.Record['D_O_B'][:4]) 
 
-        #Calculate  patient age
-        # this part extracts the year from the DOB in the record and adds to subtracts from current Year
-
-        self.Age = int(self.DateToday.year) - int(self.Record['D_O_B'][:3]) 
-
-        if self.Age < 18 :
-            self.AgeCategory = "Minor"
-        else:
-            self.AgeCategory = "Adult" 
+                if self.Age < 18 :
+                    self.AgeCategory = "Minor"
+                else:
+                    self.AgeCategory = "Adult" 
 
 
-        #BMI calculation
+                #BMI calculation
 
-        self.BMI = self.Record['weight'] /(self.Record['Height'] * self.Record['Height'])
+                self.BMI = self.Record['weight'] /(self.Record['Height'] * self.Record['Height'])
 
-        if self.BMI < 18.5:
-            self.bmiCategory = "Underweight"
-        elif self.BMI < 24.9 and self.BMI >= 18.5:
-            self.bmiCategory = "normal"
-        elif self.BMI < 29.9 and self.BMI >= 25:
-            self.bmiCategory = "OverWeight"
-        elif self.BMI >= 30:
-            self.bmiCategory = "Obessed"
+                if self.BMI < 18.5:
+                    self.bmiCategory = "Underweight"
+                elif self.BMI < 24.9 and self.BMI >= 18.5:
+                    self.bmiCategory = "normal"
+                elif self.BMI < 29.9 and self.BMI >= 25:
+                    self.bmiCategory = "OverWeight"
+                elif self.BMI >= 30:
+                    self.bmiCategory = "Obessed"
 
-        #Cholestrol Calculation
+                #Cholestrol Calculation
 
-        self.Cholestrol = self.Record['Chl_Level'] 
+                self.Cholestrol = self.Record['Chl_Level'] 
 
-        if self.Cholestrol < 200:
-            self.chlcategory = "Desirable"
-        elif self.Cholestrol< 239 and self.Cholestrol >= 200:
-            self.chlcategory = "BoarderLine"
-        elif self.Cholestrol >= 240: 
-            self.chlcategory = "High"
-
+                if self.Cholestrol < 200:
+                    self.chlcategory = "Desirable"
+                elif self.Cholestrol< 239 and self.Cholestrol >= 200:
+                    self.chlcategory = "BoarderLine"
+                elif self.Cholestrol >= 240: 
+                    self.chlcategory = "High"
+                
+                        #Health Summary
+                print("#----------------------------------#")
+                self.fullname = self.Record['first_name'] +" "+ self.Record['last_name']
+                print(f'Health Summary for {self.fullname}\n')
+                print(f'AgeCategory:{self.AgeCategory}\n')
+                print(f'BMICategory:{self.bmiCategory}\n')
+                print(f'CholestrolLevel:{self.chlcategory}\n')
+                
+                print("Would you like to enter another patient's details? Yes/No")
+                self.val = str(input())
+            
+                if self.val in ["Yes","yes","y","Y"] :
+                    self.startProgram()
+                else:
+                    print("Thank you for using the Patient Health Profile Manager & Analyzer!")
+                    print("\u00A9"+" "+"PJ SARPS Inc.")
+                    exit()         
+                    
     #function to  implement Search   
     def Search(self,Sname=" "):
         self.searchlastname = Sname
@@ -92,63 +91,69 @@ class HealthAnalyzer:
 
             #Getting Current Date
         #Get Patient Record
-        for index in range(0,len(self.PatientDB)):
-            if (self.searchlastname ) == self.PatientDB[index]['last_name']:
-                self.Record = self.PatientDB[index]
-        
+        for index in range(0,len(Patientdb)):
+           
+            if (self.searchlastname ) == Patientdb[index]['last_name']:
+                self.Record = Patientdb[index]
+                self.Age = int(self.DateToday.year) - int(self.Record['D_O_B'][:4]) 
+                
+                if self.Age < 18 :
+                    self.AgeCategory = "Minor"
+                else:
+                    self.AgeCategory = "Adult" 
+                    
+                #BMI calculation
+
+                self.BMI = self.Record['weight'] /(self.Record['Height'] * self.Record['Height'])
+
+                if self.BMI < 18.5:
+                    self.bmiCategory = "Underweight"
+                elif self.BMI < 24.9 and self.BMI >= 18.5:
+                    self.bmiCategory = "normal"
+                elif self.BMI < 29.9 and self.BMI >= 25:
+                    self.bmiCategory = "OverWeight"
+                elif self.BMI >= 30:
+                    self.bmiCategory = "Obessed"
+
+                #Cholestrol Calculation
+
+                self.Cholestrol = self.Record['Chl_Level'] 
+
+                if self.Cholestrol < 200:
+                    self.chlcategory = "Desirable"
+                elif self.Cholestrol< 239 and self.Cholestrol >= 200:
+                    self.chlcategory = "BoarderLine"
+                elif self.Cholestrol >= 240: 
+                    self.chlcategory = "High"
+
+                print("#----------------------------------#")
+                self.fullname = self.Record['first_name'] +" "+ self.Record['last_name']
+                print(f'Health Summary for {self.fullname}\n')
+                print(f'AgeCategory:{self.AgeCategory}\n')
+                print(f'BMICategory:{self.bmiCategory}\n')
+                print(f'CholestrolLevel:{self.chlcategory}\n')
+            
+                print("Would you like to enter another patient's details? Yes/No")
+                self.val = str(input())
+            
+                if self.val in ["Yes","yes","y","Y"] :
+                    self.startProgram()
+                else:
+                    print("Thank you for using the Patient Health Profile Manager & Analyzer!")
+                    print("\u00A9"+" "+"PJ SARPS Inc.")
+                    exit() 
+
             else:
-                # this Exits if users last name does not exist in Db
+              # this Exits if users last name does not exist in Db
                 print("User Not In Patient Records")
+                print("Thank you for using the Patient Health Profile Manager & Analyzer!")
+                print("\u00A9"+" "+"PJ SARPS Inc.")
                 exit
 
         #Calculate  patient age
         # this part extracts the year from the DOB in the record and adds to subtracts from current Year
 
-        self.Age = int(self.DateToday.year) - int(self.Record['D_O_B'][:3]) 
-
-        if self.Age < 18 :
-            self.AgeCategory = "Minor"
-        else:
-            self.AgeCategory = "Adult" 
-
-
-        #BMI calculation
-
-        self.BMI = self.Record['weight'] /(self.Record['Height'] * self.Record['Height'])
-
-        if self.BMI < 18.5:
-            self.bmiCategory = "Underweight"
-        elif self.BMI < 24.9 and self.BMI >= 18.5:
-            self.bmiCategory = "normal"
-        elif self.BMI < 29.9 and self.BMI >= 25:
-            self.bmiCategory = "OverWeight"
-        elif self.BMI >= 30:
-            self.bmiCategory = "Obessed"
-
-        #Cholestrol Calculation
-
-        self.Cholestrol = self.Record['Chl_Level'] 
-
-        if self.Cholestrol < 200:
-            self.chlcategory = "Desirable"
-        elif self.Cholestrol< 239 and self.Cholestrol >= 200:
-            self.chlcategory = "BoarderLine"
-        elif self.Cholestrol >= 240: 
-            self.chlcategory = "High"
-    #function to display Summary
-    def Summary(self):
-        #Health Summary
-        print("#----------------------------------#")
-        self.fullname = self.Record['first_name'] +" "+ self.Record['last_name']
-        print(f'Health Summary for {self.fullname}\n')
-        print(f'AgeCategory:{self.AgeCategory}\n')
-        print(f'BMICategory:{self.bmiCategory}\n')
-        print(f'CholestrolLevel:{self.chlcategory}\n')
-
-        print("Would you like to enter another patient's details? Yes/No")
-        self.val = str(input())
-       
-        if self.val in ["Yes","yes","y","Y"] :
+    def startProgram(self):
             print("Patient Health Profile Manager & Analyzer \n")
         
             print(f"Please choose and Option \n 1.Add new patient \n 2.Search for a patient by last name \n 3.Update patient information \n 4.Exit \n")
@@ -156,61 +161,31 @@ class HealthAnalyzer:
             print("#----------------------------------#")
             self.value = int(input())
 
-           
             if self.value == 1:
-                AddPatient.userinfo()
-                AddPatient.storeInfo()
-                AddPatient.healthAnalysis()
-                AddPatient.Summary()
+                Patient.userinfo()
+                print()
+                Patient.healthAnalysis()
+                
                 
             if self.value == 2:
-                
-                searchname = input("Enter Patient lastname \n")
-                AddPatient.Search(searchname)
-                AddPatient.Summary()
-                    
-
+                self.searchname = input("Enter Patient lastname \n")
+                Patient.Search(self.searchname)
+            
             if self.value == 3:
-                
-                AddPatient.userinfo()
-                AddPatient.storeInfo()
-                AddPatient.healthAnalysis()
-                AddPatient.Summary()
-                
-
+                Patient.userinfo()
+                print()
+                Patient.healthAnalysis()
+            
             if self.value == 4:
                 print("Thank you for using the Patient Health Profile Manager & Analyzer!")
-                exit 
-        else:   
-            print("Thank you for using the Patient Health Profile Manager & Analyzer!")
-        
-
-
+                print("\u00A9"+" "+"PJ SARPS Inc.")
+                exit()
+                 
+#class Ends Here
 
 #Code Running start Here
-print("Patient Health Profile Manager & Analyzer \n")
-        
-print(f"Please choose and Option \n 1.Add new patient  \n 2.Update patient information \n 3.Exit \n")
-
-print("#----------------------------------#")
-
-value = int(input())
-AddPatient = HealthAnalyzer()
-if value == 1:
-         AddPatient.userinfo()
-         AddPatient.storeInfo()
-         AddPatient.healthAnalysis()
-         AddPatient.Summary()
-            
-if value == 2:
-            AddPatient.userinfo()
-            AddPatient.storeInfo()
-            AddPatient.healthAnalysis()
-            AddPatient.Summary()
-            
-
-if value == 3:
-            print("Thank you for using the Patient Health Profile Manager & Analyzer!")
-            exit 
-
+#lsit for paitentreocrds
+Patientdb = []
+Patient = HealthAnalyzer()
+Patient.startProgram()
 
--- PatientSoftware.py
+++ PatientSoftware.py
@@ -1,25 +1,23 @@
 #Acessing date - time functionality
 from datetime import date
+
+Patientdb = []
 #<---------------------------------------------------------------------------->#
 #Functionality Implemented Here
 class HealthAnalyzer:
     #function to collect User Information
-    def __init__():
+    def __init__(self):
+        self.DateToday = date.today()
         pass
     def userinfo(self):
         #objective 1 Accepting user input **str mean string data type & ** int means Integer
         self.firstname = str(input("Enter Your FirstName \n"))
         self.lastname  = str(input("Enter your LastName \n"))
         self.DOB = str(input("Please Enter date of birth in YYYY-MM-DD \n"))
-        self.weight = int(input("Enter your Weight on KG \n"))
-        self.Height  = int(input("Enter your Height \n"))
-        self.ChlLevel  = int(input("Enter your Cholestrol Level \n"))
-        self.DateToday = date.today()
-        
-    
-    #function to store User Information   
-    def storeInfo(self):
-        #Storing Patient in Dictionary
+        self.weight = float(input("Enter your Weight on KG \n"))
+        self.Height  = float(input("Enter your Height \n"))
+        self.ChlLevel  = float(input("Enter your Cholestrol Level \n"))
+        #Patient Record
         self.PatientRecord = {
             "first_name" : self.firstname, 
             "last_name" : self.lastname,
@@ -28,166 +26,142 @@ class HealthAnalyzer:
             "Height" : self.Height,
             "Chl_Level" :self.ChlLevel,
         }
+        #appendinf to list
+        Patientdb.append(self.PatientRecord)
+    
+    def restartProgram(self):
+                    print(f"Please choose and Option \n 1.Add new patient \n 2.Search for a patient by last name \n 3.Update patient information \n 4.Exit \n")
 
-        # storing Records in List 
-            #creating an empty list
-        self.PatientDB = []
-        #appendidng to list 
-        self.PatientDB.append(self.PatientRecord)
-       
+                    print("#----------------------------------#")
+
+                    value = int(input())
+                    if value == 1:
+                            AddPatient.userinfo()
+                            AddPatient.healthAnalysis()
+                            
+                    if value == 2:
+                        searchnn = input("Enter Patient lastname \n")
+                        AddPatient.Search(searchnn)
+                                          
+                    if value == 3:
+                                AddPatient.userinfo()
+                                AddPatient.healthAnalysis()
+                    
+                    if value == 4:
+                                print("Thank you for using the Patient Health Profile Manager & Analyzer!")
+                                exit() 
+
+                    else:
+                     print("Thank you for using the Patient Health Profile Manager & Analyzer!")
+                     exit()         
+   
     #function perform analysis this takes a parameter that could be the search_lastname. 
     def healthAnalysis(self):
-        #Patient Health Analysis
-
-            #Getting Current Date
-        #Get Patient Record
-        for index in range(0,len(self.PatientDB)):
-            if (self.lastname ) == self.PatientDB[index]['last_name']:
-                self.Record = self.PatientDB[index]
-        
-            else:
-                # this Exits if users last name does not exist in Db
-                print("User Not In Patient Records")
-                exit
-
-        #Calculate  patient age
-        # this part extracts the year from the DOB in the record and adds to subtracts from current Year
-
-        self.Age = int(self.DateToday.year) - int(self.Record['D_O_B'][:3]) 
-
-        if self.Age < 18 :
-            self.AgeCategory = "Minor"
-        else:
-            self.AgeCategory = "Adult" 
-
-
-        #BMI calculation
-
-        self.BMI = self.Record['weight'] /(self.Record['Height'] * self.Record['Height'])
-
-        if self.BMI < 18.5:
-            self.bmiCategory = "Underweight"
-        elif self.BMI < 24.9 and self.BMI >= 18.5:
-            self.bmiCategory = "normal"
-        elif self.BMI < 29.9 and self.BMI >= 25:
-            self.bmiCategory = "OverWeight"
-        elif self.BMI >= 30:
-            self.bmiCategory = "Obessed"
-
-        #Cholestrol Calculation
-
-        self.Cholestrol = self.Record['Chl_Level'] 
-
-        if self.Cholestrol < 200:
-            self.chlcategory = "Desirable"
-        elif self.Cholestrol< 239 and self.Cholestrol >= 200:
-            self.chlcategory = "BoarderLine"
-        elif self.Cholestrol >= 240: 
-            self.chlcategory = "High"
-
-    #function to  implement Search   
-    def Search(self,Sname=" "):
-        self.searchlastname = Sname
-        #Patient Health Analysis
-
-            #Getting Current Date
-        #Get Patient Record
-        for index in range(0,len(self.PatientDB)):
-            if (self.searchlastname ) == self.PatientDB[index]['last_name']:
-                self.Record = self.PatientDB[index]
-        
-            else:
-                # this Exits if users last name does not exist in Db
-                print("User Not In Patient Records")
-                exit
-
-        #Calculate  patient age
-        # this part extracts the year from the DOB in the record and adds to subtracts from current Year
-
-        self.Age = int(self.DateToday.year) - int(self.Record['D_O_B'][:3]) 
-
-        if self.Age < 18 :
-            self.AgeCategory = "Minor"
-        else:
-            self.AgeCategory = "Adult" 
-
-
-        #BMI calculation
-
-        self.BMI = self.Record['weight'] /(self.Record['Height'] * self.Record['Height'])
-
-        if self.BMI < 18.5:
-            self.bmiCategory = "Underweight"
-        elif self.BMI < 24.9 and self.BMI >= 18.5:
-            self.bmiCategory = "normal"
-        elif self.BMI < 29.9 and self.BMI >= 25:
-            self.bmiCategory = "OverWeight"
-        elif self.BMI >= 30:
-            self.bmiCategory = "Obessed"
-
-        #Cholestrol Calculation
-
-        self.Cholestrol = self.Record['Chl_Level'] 
-
-        if self.Cholestrol < 200:
-            self.chlcategory = "Desirable"
-        elif self.Cholestrol< 239 and self.Cholestrol >= 200:
-            self.chlcategory = "BoarderLine"
-        elif self.Cholestrol >= 240: 
-            self.chlcategory = "High"
-    #function to display Summary
-    def Summary(self):
-        #Health Summary
-        print("#----------------------------------#")
-        self.fullname = self.Record['first_name'] +" "+ self.Record['last_name']
-        print(f'Health Summary for {self.fullname}\n')
-        print(f'AgeCategory:{self.AgeCategory}\n')
-        print(f'BMICategory:{self.bmiCategory}\n')
-        print(f'CholestrolLevel:{self.chlcategory}\n')
-
-        print("Would you like to enter another patient's details? Yes/No")
-        self.val = str(input())
-       
-        if self.val in ["Yes","yes","y","Y"] :
-            print("Patient Health Profile Manager & Analyzer \n")
-        
-            print(f"Please choose and Option \n 1.Add new patient \n 2.Search for a patient by last name \n 3.Update patient information \n 4.Exit \n")
-           
-            print("#----------------------------------#")
-            self.value = int(input())
-
-           
-            if self.value == 1:
-                AddPatient.userinfo()
-                AddPatient.storeInfo()
-                AddPatient.healthAnalysis()
-                AddPatient.Summary()
-                
-            if self.value == 2:
-                
-                searchname = input("Enter Patient lastname \n")
-                AddPatient.Search(searchname)
-                AddPatient.Summary()
                     
+                self.Record = self.PatientRecord
+                self.Age = int(self.DateToday.year) - int(self.Record['D_O_B'][:4]) 
 
-            if self.value == 3:
+                if self.Age < 18 :
+                    self.AgeCategory = "Minor"
+                else:
+                    self.AgeCategory = "Adult" 
+
+
+                #BMI calculation
+
+                self.BMI = self.Record['weight'] /(self.Record['Height'] * self.Record['Height'])
+
+                if self.BMI < 18.5:
+                    self.bmiCategory = "Underweight"
+                elif self.BMI < 24.9 and self.BMI >= 18.5:
+                    self.bmiCategory = "normal"
+                elif self.BMI < 29.9 and self.BMI >= 25:
+                    self.bmiCategory = "OverWeight"
+                elif self.BMI >= 30:
+                    self.bmiCategory = "Obessed"
+
+                #Cholestrol Calculation
+
+                self.Cholestrol = self.Record['Chl_Level'] 
+
+                if self.Cholestrol < 200:
+                    self.chlcategory = "Desirable"
+                elif self.Cholestrol< 239 and self.Cholestrol >= 200:
+                    self.chlcategory = "BoarderLine"
+                elif self.Cholestrol >= 240: 
+                    self.chlcategory = "High"
                 
-                AddPatient.userinfo()
-                AddPatient.storeInfo()
-                AddPatient.healthAnalysis()
-                AddPatient.Summary()
+                        #Health Summary
+                print("#----------------------------------#")
+                self.fullname = self.Record['first_name'] +" "+ self.Record['last_name']
+                print(f'Health Summary for {self.fullname}\n')
+                print(f'AgeCategory:{self.AgeCategory}\n')
+                print(f'BMICategory:{self.bmiCategory}\n')
+                print(f'CholestrolLevel:{self.chlcategory}\n')
                 
+                print("Would you like to enter another patient's details? Yes/No")
+                self.val = str(input())
+            
+                if self.val in ["Yes","yes","y","Y"] :
+                    self.restartProgram()
+    #function to  implement Search   
+    def Search(self,Sname=""):
+        #Get Patient Record
+        for index in range(0,len(Patientdb)):
+            print (Sname)
+            if (Sname) == Patientdb[index]['last_name'] :
+                self.Record = Patientdb[index]
+                self.Age = int(self.DateToday.year) - int(self.Record['D_O_B'][:4]) 
+                
+                if self.Age < 18 :
+                    self.AgeCategory = "Minor"
+                else:
+                    self.AgeCategory = "Adult" 
+                    
+                #BMI calculation
 
-            if self.value == 4:
-                print("Thank you for using the Patient Health Profile Manager & Analyzer!")
-                exit 
-        else:   
-            print("Thank you for using the Patient Health Profile Manager & Analyzer!")
-        
+                self.BMI = self.Record['weight'] /(self.Record['Height'] * self.Record['Height'])
 
+                if self.BMI < 18.5:
+                    self.bmiCategory = "Underweight"
+                elif self.BMI < 24.9 and self.BMI >= 18.5:
+                    self.bmiCategory = "normal"
+                elif self.BMI < 29.9 and self.BMI >= 25:
+                    self.bmiCategory = "OverWeight"
+                elif self.BMI >= 30:
+                    self.bmiCategory = "Obessed"
 
+                #Cholestrol Calculation
+
+                self.Cholestrol = self.Record['Chl_Level'] 
+
+                if self.Cholestrol < 200:
+                    self.chlcategory = "Desirable"
+                elif self.Cholestrol< 239 and self.Cholestrol >= 200:
+                    self.chlcategory = "BoarderLine"
+                elif self.Cholestrol >= 240: 
+                    self.chlcategory = "High"
+
+                print("#----------------------------------#")
+                self.fullname = self.Record['first_name'] +" "+ self.Record['last_name']
+                print(f'Health Summary for {self.fullname}\n')
+                print(f'AgeCategory:{self.AgeCategory}\n')
+                print(f'BMICategory:{self.bmiCategory}\n')
+                print(f'CholestrolLevel:{self.chlcategory}\n')
+            
+                print("Would you like to enter another patient's details? Yes/No")
+                self.val = str(input())
+            
+                if self.val in ["Yes","yes","y","Y"] :
+                    self.restartProgram()
+         # this Exits if users last name does not exist in Db
+        print("User Not In Patient Records")
+        print("Thank you for using the Patient Health Profile Manager & Analyzer!")
+        exit()
+#end of class
 
 #Code Running start Here
+AddPatient = HealthAnalyzer()
 print("Patient Health Profile Manager & Analyzer \n")
         
 print(f"Please choose and Option \n 1.Add new patient  \n 2.Update patient information \n 3.Exit \n")
@@ -195,22 +169,19 @@ print(f"Please choose and Option \n 1.Add new patient  \n 2.Update patient infor
 print("#----------------------------------#")
 
 value = int(input())
-AddPatient = HealthAnalyzer()
+
 if value == 1:
          AddPatient.userinfo()
-         AddPatient.storeInfo()
          AddPatient.healthAnalysis()
-         AddPatient.Summary()
+        
             
 if value == 2:
             AddPatient.userinfo()
-            AddPatient.storeInfo()
             AddPatient.healthAnalysis()
-            AddPatient.Summary()
             
 
 if value == 3:
             print("Thank you for using the Patient Health Profile Manager & Analyzer!")
-            exit 
+            exit() 
 
 
